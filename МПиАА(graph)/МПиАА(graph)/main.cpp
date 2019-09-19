4dde7ee5d64ddd99aa1c98f1eebe5bab26241b19
Ch0knutiy <55545003+Ch0knutiy@users.noreply.github.com>
2019-09-20 00:21:04 +0700

    git add

---
+++ МПиАА(graph)/МПиАА(graph)/main.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <vector>
+#include <stack>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+   int n, m, v1, v2, l;
+   srand(time(0));
+   clock_t start, end;
+   ifstream fin;
+   ofstream fout;
+   fout.open("ou.txt", ios::out);
+   fin.open("in.txt", ios::in);
+   fin >> n >> m;
+   vector< vector < int > > g(n);
+   for (int i = 0; i < m; i++)
+   {
+      fin >> v1 >> v2;
+      g[v1 - 1].push_back(v2 - 1);
+      g[v2 - 1].push_back(v1 - 1);
+   }
+   int s = 0;
+   vector<int> p(n, 0);
+   stack  <int> q;
+   q.push(s);
+   while (!q.empty())
+   {
+      l = q.top();
+      q.pop();
+      if (p[l] == 0)
+      {
+         p[l] = 1;
+         fout << l << ' ';
+         for (int i = 0; i < g[l].size(); i++)
+            if (p[g[l][i]] == 0)
+               q.push(g[l][i]);
+      }
+   }
+}
